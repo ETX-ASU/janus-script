852985213899d01ba8b77f2ce91956b19025159e
Warspawn <bsparks42@gmail.com>
2021-03-30 11:47:42 -0700

    add 'rand' function to builtins for random numbers

--- src/lib/eval/builtin.ts
+++ src/lib/eval/builtin.ts
@@ -22,16 +22,50 @@ const builtinRound: BuiltInFunction = (input: EnvObj) => {
 
 const builtinJSON: BuiltInFunction = (input: EnvObj) => {
     return new StringObj(input.toJSON());
-}
+};
 
 const builtinTypeof: BuiltInFunction = (input: EnvObj) => {
     return new StringObj(input.Type().toString());
-}
+};
+
+const isInt = (x) => Math.floor(x) === x;
+const builtinRand: BuiltInFunction = (min: EnvObj, max: EnvObj) => {
+    if (!min && !max) {
+        return new NumberObj(Math.random());
+    }
+    if (min && min.Type() !== EnvObjType.NUMBER) {
+        return new ErrorObj(
+            `(min) expected NUMBER got ${min.Type().toString()}`
+        );
+    }
+    if (max && max.Type() !== EnvObjType.NUMBER) {
+        return new ErrorObj(
+            `(max) expected NUMBER got ${max.Type().toString()}`
+        );
+    }
+    let minValue = 0;
+    let maxValue = 0;
+    if (!max) {
+        maxValue = (min as NumberObj).Value;
+    } else {
+        minValue = (min as NumberObj).Value;
+        maxValue = (max as NumberObj).Value;
+    }
+    let result;
+    if (isInt(minValue) && isInt(maxValue)) {
+        result = Math.floor(Math.random() * (maxValue - minValue) + minValue);
+    } else {
+        result = Math.random() * (maxValue - minValue) + minValue;
+    }
+
+    return new NumberObj(result);
+};
 
 export const builtins: Record<string, EnvObj> = {
     null: NULL,
     undefined: UNDEFINED,
     round: new BuiltInFunctionObj(builtinRound),
+    rand: new BuiltInFunctionObj(builtinRand),
     json: new BuiltInFunctionObj(builtinJSON),
     typeof: new BuiltInFunctionObj(builtinTypeof),
     'Math.PI': new NumberObj(Math.PI),
